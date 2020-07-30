5c95b801c372911a747762e165a9ea82e501680b
mak art <amin.khan@aacn.org>
2020-07-30 08:20:34 -0700

    REAL START

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import { mountDOM } from "./components/mountdom.js";
 const root = document.querySelector("#root");
 
 const myel = document.createElement("h1");
-myel.innerHTML = "hello from non react component!";
+myel.innerHTML = "hello from NON react component!";
 
 mountDOM(myel, root);
